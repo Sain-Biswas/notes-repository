8d793dfbcf9cf80eb8a2797a59ac1845408aa419
Sain-Biswas <sain.biswas614@gmail.com>
2025-05-18 18:10:50 +0530

    chore(social): add social links - github, linkedin, website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,42 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "My Docs",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/Sain-Biswas",
+        },
+        {
+          icon: "linkedin",
+          label: "Linkedin",
+          href: "https://linkedin.com/in/sainbiswas",
+        },
+        {
+          icon: "seti:react",
+          label: "Portfolio",
+          href: "https://sainbiswas.vercel.app",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
